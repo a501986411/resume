5db6ee05397ad06ad1e931d9896806ac830caf3b
501986411@qq.com <501986411@qq.com>
2017-08-10 17:04:46 +0800

    登录页面 v.0.1

--- runtime/temp/5a17311cb73eb990f25733f13a0a076e.php
+++ runtime/temp/5a17311cb73eb990f25733f13a0a076e.php
@@ -1,23 +1,70 @@
-<?php if (!defined('THINK_PATH')) exit(); /*a:1:{s:67:"E:\project\resume\public/../application/index\view\index\Login.html";i:1502271368;}*/ ?>
+<?php if (!defined('THINK_PATH')) exit(); /*a:1:{s:67:"E:\project\resume\public/../application/index\view\index\Login.html";i:1502349363;}*/ ?>
 <!DOCTYPE html>
-<html lang="en">
+<html class="login-alone">
 <head>
-    <meta charset="UTF-8">
-    <title>简历共享-用户登录</title>
-    <link rel="stylesheet" href="<?php echo UNIT_PATH; ?>layui/css/layui.css" media="all">
-    <style>
-        html,body{height: 100%;width: 100%;overflow: auto;}
-        body{padding: 0;margin: 0;}
-    </style>
+    <title>简历管理</title>
+    <meta http-equiv="content-type" content="text/html; charset=UTF-8" />
+
+    <link href="<?php echo STATIC_PATH; ?>/index/login/ui/css/screen.css?v=3.9" media="screen, projection" rel="stylesheet" type="text/css" >
+    <link rel="stylesheet" type="text/css" href="<?php echo STATIC_PATH; ?>/index/login/ui/css/base.css?v=3.9">
+    <link rel="stylesheet" type="text/css" href="<?php echo STATIC_PATH; ?>/index/login/passport/css/login.css?v=3.9">
+
 </head>
-<body >
-    <div style="height: 100%;width: 100%;">
-        <ul class="layui-nav" lay-filter="">
-            <li class="layui-nav-item"><a href="">最新活动</a></li>
-        </ul>
+<body>
+<div class="logina-logo" style="height: 55px">
+    <a href="">
+        <img src="<?php echo STATIC_PATH; ?>/index/login/passport/images/toplogo.png?v=3.9" height="60" alt="">
+    </a>
+</div>
+<div class="logina-main main clearfix">
+    <div class="tab-con">
+        <form id="form-login" method="post" action="passport/ajax-login">
+            <div id='login-error' class="error-tip"></div>
+            <table border="0" cellspacing="0" cellpadding="0">
+                <tbody>
+                <tr>
+                    <th>账户</th>
+                    <td width="245">
+                        <input id="email" type="text" name="email" placeholder="电子邮箱" autocomplete="off" value=""></td>
+                    <td>
+                    </td>
+                </tr>
+                <tr>
+                    <th>密码</th>
+                    <td width="245">
+                        <input id="password" type="password" name="password" placeholder="请输入密码" autocomplete="off">
+                    </td>
+                    <td>
+                    </td>
+                </tr>
+                <tr class="find">
+                    <th></th>
+                    <td>
+                        <div>
+                            <label class="checkbox" for="chk11"><input style="height: auto;" id="chk11" type="checkbox" name="remember_me" >记住我</label>
+                            <a href="passport/forget-pwd">忘记密码？</a>
+                        </div>
+                    </td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <th></th>
+                    <td width="245"><input class="confirm" type="submit" value="登  录"></td>
+                    <td></td>
+                </tr>
+                </tbody>
+            </table>
+            <input type="hidden" name="refer" value="site/">
+        </form>
     </div>
+    <div class="reg">
+        <p>还没有账号？<br>赶快免费注册一个吧！</p>
+        <a class="reg-btn" href="#">立即免费注册</a>
+    </div>
+</div>
+<div id="footer">
+    <div class="copyright">Copyright © 2014 js-css.cn. All Rights Reserved. </div>
+</div>
+
 </body>
-<script src="<?php echo UNIT_PATH; ?>vue.min.js"></script>
-<script src="<?php echo UNIT_PATH; ?>layui/layui.js"></script>
-<script src="<?php echo STATIC_PATH; ?>index/js/load_layui_module.js"></script>
 </html>
