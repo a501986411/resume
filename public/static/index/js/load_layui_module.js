a6d06a826b5d5d3fec9253950e19aa36b4ba4518
501986411@qq.com <501986411@qq.com>
2017-08-14 16:05:02 +0800

    简历列表

--- public/static/index/js/load_layui_module.js
+++ public/static/index/js/load_layui_module.js
@@ -5,9 +5,13 @@ var layui_module = {};
 layui.use([
     'form',
     'element',
+    'laypage',
     'layer',
+
 ],function(){
-    layui_module.elem = layui.element();
+    layui_module.elem = layui.element;
     layui_module.layer = layui.layer;
-    layui_module.form = layui.form();
+    layui_module.form = layui.form;
+    layui_module.page =  layui.laypage;
 });
+
--- resume.sql
+++ resume.sql
@@ -2,15 +2,15 @@
 Navicat MySQL Data Transfer
 
 Source Server         : 127.0.0.1
-Source Server Version : 50714
+Source Server Version : 50617
 Source Host           : localhost:3306
 Source Database       : resume
 
 Target Server Type    : MYSQL
-Target Server Version : 50714
+Target Server Version : 50617
 File Encoding         : 65001
 
-Date: 2017-08-13 21:02:34
+Date: 2017-08-14 16:04:20
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -20,20 +20,32 @@ SET FOREIGN_KEY_CHECKS=0;
 -- ----------------------------
 DROP TABLE IF EXISTS `cv_member`;
 CREATE TABLE `cv_member` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL,
   `email` varchar(255) NOT NULL DEFAULT '' COMMENT '邮箱',
   `nick_name` varchar(255) NOT NULL DEFAULT '' COMMENT '昵称',
   `password` varchar(255) NOT NULL DEFAULT '' COMMENT '密码',
   `salt` varchar(255) NOT NULL DEFAULT '' COMMENT '昵称全拼用于加密',
-  `type` tinyint(1) unsigned NOT NULL DEFAULT '1' COMMENT '会员类型1：普通会员，2：企业会员',
-  `register_time` int(11) NOT NULL DEFAULT '0' COMMENT '注册时间',
-  `last_login_ip` varchar(11) NOT NULL DEFAULT '' COMMENT '最后一次登录IP',
   PRIMARY KEY (`id`),
   UNIQUE KEY `email` (`email`) USING BTREE
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8 COMMENT='会员基础表';
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='会员基础表';
 
 -- ----------------------------
 -- Records of cv_member
 -- ----------------------------
-INSERT INTO `cv_member` VALUES ('1', '501986411@qq.com', '噢，我想多了', '2a73071fffec0ea79d2659bcdf2c81e8', 'chenhailong', '1', '1502601963', '127.0.0.1');
-INSERT INTO `cv_member` VALUES ('7', '991738189@qq.com', '噢，我想多了4', 'd8526b61f69720d007b5013dfff681f2', '7wmw0m3o6', '1', '1502601963', '127.0.0.1');
+INSERT INTO `cv_member` VALUES ('1', '501986411@qq.com', '噢，我想多了', '2a73071fffec0ea79d2659bcdf2c81e8', 'chenhailong');
+
+-- ----------------------------
+-- Table structure for cv_resume_module
+-- ----------------------------
+DROP TABLE IF EXISTS `cv_resume_module`;
+CREATE TABLE `cv_resume_module` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '主键',
+  `name` varchar(255) CHARACTER SET utf8 NOT NULL DEFAULT '' COMMENT '模块名称',
+  `status` tinyint(11) NOT NULL DEFAULT '1' COMMENT '1启用，0：停用',
+  `is_must` tinyint(11) NOT NULL DEFAULT '0' COMMENT '0：选填，1.必填',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COMMENT='简历内容没款配置';
+
+-- ----------------------------
+-- Records of cv_resume_module
+-- ----------------------------
